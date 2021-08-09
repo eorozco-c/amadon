aea620175cd85d1ca785d462b3ea308d431a755d
Ernesto Orozco C <ernestorozco94@gmail.com>
2021-08-09 02:10:49 -0400

    first commit

---
+++ poorly_coded_store/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import redirect, render
+from .models import Order, Product
+
+def index(request):
+    context = {
+        "all_products": Product.objects.all()
+    }
+    return render(request, "store/index.html", context)
+
+def checkout(request):
+    if request.method == "POST":
+        quantity_from_form = int(request.POST["quantity"])
+        try:
+            product = Product.objects.get(id=request.POST["price"])
+            price_from_form = float(product.price)
+        except:
+            return redirect("/")
+        total_charge = quantity_from_form * price_from_form
+        if "cantidad" in request.session:
+            request.session["cantidad"] += quantity_from_form
+            request.session["gasto"] += price_from_form
+        else:
+            request.session["cantidad"] = quantity_from_form
+            request.session["gasto"] = price_from_form
+        request.session["total"] = total_charge
+        context = {
+            "cantidad" : request.session["cantidad"],
+            "gasto" : request.session["gasto"],
+            "total" : request.session["total"],
+        }
+        Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+        return redirect("/checkout")
+    elif request.method == "GET":
+        context = {
+            "cantidad" : request.session["cantidad"],
+            "gasto" : request.session["gasto"],
+            "total" : request.session["total"],
+        }
+        return render(request, "store/checkout.html", context)
